2a7ff288b1f8290eabcb90d541859e896eb17b68
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-06-24 19:49:55 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "why_";
 
 client.on('ready', () => {
     console.log('I am ready!')
@@ -7,49 +8,49 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'why_ping') {
+    if (message.content === prefix + 'ping') {
     	message.channel.send('PONG!');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'why_pong') {
+    if (message.content === prefix + 'pong') {
     	message.channel.send('Ping!');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'why_about') {
+    if (message.content === prefix + 'about') {
     	message.channel.send('WhyBot by JPlexer Version: 0.3.1');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'why_help') {
-    	message.channel.send('```why_ping/pong (You Know what it is!) why_rps rock/paper/scissors/ (Play Rock Paper Scissors) why_about(You Know what it is!)```');
+    if (message.content === prefix + 'help') {
+    	message.channel.send('```prefix + ping/pong (You Know what it is!) prefix + rps rock/paper/scissors/ (Play Rock Paper Scissors) prefix + about(You Know what it is!)```');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'why_rps rock') {
+    if (message.content === prefix + 'rps rock') {
     	message.channel.send('Paper! I won.');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'why_rps paper') {
+    if (message.content === prefix + 'rps paper') {
     	message.channel.send('Scissors! I won.');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'why_rps scissors') {
+    if (message.content === prefix + 'rps scissors') {
     	message.channel.send('Rock! I won.');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'why_lol') {
+    if (message.content === prefix + 'lol') {
     	message.channel.send(':scream: You found the Secret :scream:');
   	}
 });
