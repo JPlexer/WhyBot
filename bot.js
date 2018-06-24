99e24a5ae9da6345b5aad37f4edccbaa4c98abe8
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-06-24 19:54:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,63 +2,67 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "why_";
 
-msg = message.content.toLowerCase();
-
 client.on('ready', () => {
     console.log('I am ready!')
     client.user.setGame('why_help')
 });
 
 client.on('message', message => {
-    if (msg.startsWith === prefix + 'ping') {
+    if (message.content === prefix + 'ping') {
     	message.channel.send('PONG!');
   	}
 });
 
 client.on('message', message => {
-    if (msg.startsWith === prefix + 'pong') {
+    if (message.content === prefix + 'pong') {
     	message.channel.send('Ping!');
   	}
 });
 
 client.on('message', message => {
-    if (msg.startsWith === prefix + 'about') {
+    if (message.content === prefix + 'about') {
     	message.channel.send('WhyBot by JPlexer Version: 0.3.1');
   	}
 });
 
 client.on('message', message => {
-    if (msg.startsWith === prefix + 'help') {
-    	message.channel.send('```why_ping/pong (You Know what it is!) why_rps rock/paper/scissors/ (Play Rock Paper Scissors) why_about(You Know what it is!)```');
+    if (message.content === prefix + 'help') {
+    	message.channel.send('```prefix + ping/pong (You Know what it is!) prefix + rps rock/paper/scissors/ (Play Rock Paper Scissors) prefix + about(You Know what it is!)```');
   	}
 });
 
 client.on('message', message => {
-    if (msg.startsWith === prefix + 'rps rock') {
+    if (message.content === prefix + 'rps rock') {
     	message.channel.send('Paper! I won.');
   	}
 });
 
 client.on('message', message => {
-    if (msg.startsWith === prefix + 'rps paper') {
+    if (message.content === prefix + 'rps paper') {
     	message.channel.send('Scissors! I won.');
   	}
 });
 
 client.on('message', message => {
-    if (msg.startsWith === prefix + 'rps scissors') {
+    if (message.content === prefix + 'rps scissors') {
     	message.channel.send('Rock! I won.');
   	}
 });
 
 client.on('message', message => {
-    if (msg.startsWith === prefix + 'lol') {
+    if (message.content === prefix + 'lol') {
     	message.channel.send(':scream: You found the Secret :scream:');
   	}
 });
 
 client.on('message', message => {
-    if (msg.startsWith === 'o') {
+    if (message.content === 'o') {
+    	message.channel.send('Oh or Ok??');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'O') {
     	message.channel.send('Oh or Ok??');
   	}
 });
